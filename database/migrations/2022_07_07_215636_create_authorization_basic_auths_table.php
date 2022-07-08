b850c512c4a050b4d88e63dada8a86215e1b32d6
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-07-08 16:36:12 -0300

    Migrations Authorization Basic Auth

---
+++ database/migrations/2022_07_07_215636_create_authorization_basic_auths_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authorization_basic_auths', function (Blueprint $table) {
+            $table->id();
+            $table->string('app', 255);
+            $table->bigInteger('store_id')->nullable();
+            $table->string('username', 255);
+            $table->string('password', 255);
+            $table->string('basic', 255);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authorization_basic_auths');
+    }
+};
