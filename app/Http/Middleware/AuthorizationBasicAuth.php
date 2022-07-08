0e848d28bfebddf25e6dd5f587be608ff7fe4935
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-07-08 16:28:20 -0300

    middleware authorization basic auth

---
+++ app/Http/Middleware/AuthorizationBasicAuth.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\AuthorizationBasicAuth as AuthorizationBasicAuthModel;
+
+class AuthorizationBasicAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $basic_auth = $request->header('authorization');
+        $app = AuthorizationBasicAuthModel::firstWhere('basic', $basic_auth);
+        if ($app->basic == $basic_auth) {
+            return $next($request);
+        } else {
+            return response()->json([
+                "errors" => "[API] Invalid Username provided for Basic Auth API access"
+            ], 401);
+        }
+    }
+}
