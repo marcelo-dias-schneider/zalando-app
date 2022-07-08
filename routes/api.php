238b21ad35b1ba4df697235d7d7f134549884339
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-07-08 16:29:29 -0300

    api route orders

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('authorization.basic.auth')->post('orders', function (Request $request) {
+    return response()->json([
+        'success' => 'the authentication works',
+    ]);    
+});
