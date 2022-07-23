12c68d612b1c1b9072b6f83e5aedc4e0e696a788
Marcelo Dias Schneider <marcelo.d.schneider@gmail.com>
2022-07-23 10:58:25 -0300

    migration add foreign key

---
+++ database/migrations/2022_07_22_194358_alter_table_zalando_customer_order_notifications_add_foreign_key_basic_auth_id.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('zalando_order_notifications', function (Blueprint $table) {
+            $table->unsignedBigInteger('authorization_basic_auth_id');
+            $table->foreign(
+                'authorization_basic_auth_id',
+                'order_notifications_authorization_basic_auth_id_foreign'
+            )
+                ->references('id')
+                ->on('authorization_basic_auths');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('zalando_order_notifications', function (Blueprint $table) {
+            $table->dropColumn('authorization_basic_auth_id');
+        });
+    }
+};
